576459e85831c0e18d85ad08f7e1e8e6f20dea03
Lav <lavhuyaar@gmail.com>
2024-09-05 20:58:02 +0530

    feat: analyzeArray function updated and tested

--- testing/analyzeArray.js
+++ testing/analyzeArray.js
@@ -1,5 +1,10 @@
 function analyzeArray(arr) {
-    return arr
+  return {
+    average: arr.reduce((prev, curr) => prev + curr / arr.length, 0),
+    min: arr.reduce((prev, curr) => Math.min(prev, curr), arr[0]),
+    max: arr.reduce((prev, curr) => Math.max(prev, curr), arr[0]),
+    length: arr.length,
+  };
 }
 
 module.exports = analyzeArray;
--- testing/main.test.js
+++ testing/main.test.js
@@ -1,29 +1,31 @@
-// //Capitalize test
-// const capitalize = require("./capitalize");
-// test("Capitalized string", () => expect(capitalize("jupiter")).toBe("Jupiter"));
+//Capitalize test
+const capitalize = require("./capitalize");
+test("Capitalized string", () => expect(capitalize("jupiter")).toBe("Jupiter"));
 
-// //Reversed String test
-// const reverseString = require("./reverseString");
-// test("Reversed string", () => expect(reverseString("Baby")).toBe("ybaB"));
+//Reversed String test
+const reverseString = require("./reverseString");
+test("Reversed string", () => expect(reverseString("Baby")).toBe("ybaB"));
 
-// //Calculator test
-// const calculator = require("./calculator");
-// test("Calculator add", () => expect(calculator.add(8, 4)).toBe(12));
-// test("Calculator subtract", () => expect(calculator.subtract(8, 4)).toBe(4));
-// test("Calculator multiply", () => expect(calculator.multiply(8, 4)).toBe(32));
-// test("Calculator divide", () => expect(calculator.divide(8, 4)).toBe(2));
+//Calculator test
+const calculator = require("./calculator");
+test("Calculator add", () => expect(calculator.add(8, 4)).toBe(12));
+test("Calculator subtract", () => expect(calculator.subtract(8, 4)).toBe(4));
+test("Calculator multiply", () => expect(calculator.multiply(8, 4)).toBe(32));
+test("Calculator divide", () => expect(calculator.divide(8, 4)).toBe(2));
 
 //Caesar Cipher test
 const caesarCipher = require("./caesarCipher");
-test("Caesar Cipher 1", () => expect(caesarCipher('xyz', 3)).toBe('abc'))
-test("Caesar Cipher 2", () => expect(caesarCipher('HeLLo', 3)).toBe('KhOOr'))
-test("Caesar Cipher 3", () => expect(caesarCipher('Hello, World!', 3)).toBe('Khoor, Zruog!'))
+test("Caesar Cipher 1", () => expect(caesarCipher("xyz", 3)).toBe("abc"));
+test("Caesar Cipher 2", () => expect(caesarCipher("HeLLo", 3)).toBe("KhOOr"));
+test("Caesar Cipher 3", () =>
+  expect(caesarCipher("Hello, World!", 3)).toBe("Khoor, Zruog!"));
 
 //Analyze array test
-const analyzeArray = require('./analyzeArray')
-test("Analyze array", () => expect(analyzeArray([1,8,3,4,2,6])).toBe(object == {
+const analyzeArray = require("./analyzeArray");
+test("Analyze array", () =>
+  expect(analyzeArray([1, 8, 3, 4, 2, 6])).toEqual({
     average: 4,
     min: 1,
     max: 8,
-    length: 6
- }))
+    length: 6,
+  }));
