71f9f0764e9e9b0c8bd16cf510d4719ef76dbfbd
Lav <lavhuyaar@gmail.com>
2024-09-05 16:19:40 +0530

    feat: caesarCipher function created

---
+++ testing/caesarCipher.js
@@ -0,0 +1,5 @@
+function caesarCipher() {
+    return null;
+}
+
+module.exports = caesarCipher;
--- testing/main.test.js
+++ testing/main.test.js
@@ -7,8 +7,14 @@ const reverseString = require("./reverseString");
 test("Reversed string", () => expect(reverseString("Baby")).toBe("ybaB"));
 
 //Calculator test
-const calculator = require('./calculator');
-test('Calculator add', () => expect(calculator.add(8, 4)).toBe(12))
-test('Calculator subtract', () => expect(calculator.subtract(8, 4)).toBe(4))
-test('Calculator multiply', () => expect(calculator.multiply(8, 4)).toBe(32))
-test('Calculator divide', () => expect(calculator.divide(8, 4)).toBe(2))
+const calculator = require("./calculator");
+test("Calculator add", () => expect(calculator.add(8, 4)).toBe(12));
+test("Calculator subtract", () => expect(calculator.subtract(8, 4)).toBe(4));
+test("Calculator multiply", () => expect(calculator.multiply(8, 4)).toBe(32));
+test("Calculator divide", () => expect(calculator.divide(8, 4)).toBe(2));
+
+//Caesar Cipher test
+const caesarCipher = require("./caesarCipher");
+test("Caesar Cipher", () => expect(caesarCipher('xyz', 3)).toBe('abc'))
+test("Caesar Cipher", () => expect(caesarCipher('HeLLo', 3)).toBe('KhOOr'))
+test("Caesar Cipher", () => expect(caesarCipher('Hello, World!', 3)).toBe('Khoor, Zruog!'))
