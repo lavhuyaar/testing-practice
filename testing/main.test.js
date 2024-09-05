db001ee9830b6a31be3220e97a3e545284a43899
Lav <lavhuyaar@gmail.com>
2024-09-05 16:13:45 +0530

    feat: Calculator object (with add, subtract, multiply and divide functions) added and tested

--- testing/main.test.js
+++ testing/main.test.js
@@ -5,3 +5,10 @@ test("Capitalized string", () => expect(capitalize("jupiter")).toBe("Jupiter"));
 //Reversed String test
 const reverseString = require("./reverseString");
 test("Reversed string", () => expect(reverseString("Baby")).toBe("ybaB"));
+
+//Calculator test
+const calculator = require('./calculator');
+test('Calculator add', () => expect(calculator.add(8, 4)).toBe(12))
+test('Calculator subtract', () => expect(calculator.subtract(8, 4)).toBe(4))
+test('Calculator multiply', () => expect(calculator.multiply(8, 4)).toBe(32))
+test('Calculator divide', () => expect(calculator.divide(8, 4)).toBe(2))
